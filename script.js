62ecf096498aebd30fcb93a3fff013710ff5b069
SatyamK <55749802+devCadet1@users.noreply.github.com>
2023-06-28 23:25:46 +0530

    error1 script.js

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ let signupBtn = document.getElementById('signupBtn')
 let title = document.getElementById('title')
 let nameField = document.getElementById('name-field')
 
+/*
 signinBtn.onclick = function () {
     nameField.style.maxHeight = '0'
     title.innerHTML = 'Sign In'
     signinBtn.classList.remove('disabled')
     signupBtn.classList.add('disabled')
+*/
 // this piece of code isn't working 
 }
 
